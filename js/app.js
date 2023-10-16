e17aa9d1c5641e57fce5d3316fbc2cca09159733
igorliotta <liottaigor@gmail.com>
2023-10-16 17:45:30 +0200

    Added: bombsArray

--- js/app.js
+++ js/app.js
@@ -10,6 +10,8 @@ btnPlayElement.addEventListener('click', function () {
     // Serve per svuotare la griglia
     gridElement.innerHTML = '';
 
+    getArrayOfIntRandomNumber();
+
     // Serve a generare le caselle all'interno della griglia
     for (let i = 0; i < 100; i++) {
         n = i + 1;
@@ -28,24 +30,31 @@ btnPlayElement.addEventListener('click', function () {
    
     //Cicliamo la nodelist dei box    
    for (let i = 0; i < boxDOMElements.length; i++) {
-    const currentBoxElement = boxDOMElements[i];
-    // console.log(currentBoxElement);
+        const currentBoxElement = boxDOMElements[i];
+        // console.log(currentBoxElement);
 
-    // Ad ogni box aggiungo un eventListner
-    currentBoxElement.addEventListener('click', function() {
+        // Ad ogni box aggiungo un eventListner
+        currentBoxElement.addEventListener('click', function() {
 
-        // Stampo il riferimento al numero del box cliccato
-        console.log('Ho cliccato nella casella numero: ' + [i + 1]);
+            // Stampo il riferimento al numero del box cliccato
+            console.log('Ho cliccato nella casella numero: ' + [i + 1]);
 
-    currentBoxElement.classList.add('bg-light-blue');
-    })
-   }
+            currentBoxElement.classList.add('bg-light-blue');
+        })
+    }
+
+
 })
+
+let bombs = getArrayOfIntRandomNumber(16);
 
 function getArrayOfIntRandomNumber() {
    
     const bombsArray = [];
-    while (bombsArray.lenght < 16) {
+    console.log("getArrayOfIntRandomNumber > Empty bombs array", bombsArray);
+
+    while (bombsArray.length < 16) {
+
         const number = getRandomIntInclusive(1, 100);
         console.log(bombsArray.includes(number));
 
@@ -54,7 +63,30 @@ function getArrayOfIntRandomNumber() {
         }
     }
 
+    console.log("Full bombs array", bombsArray);
     return bombsArray;
+}
+
+for (let i = 0; i < bombs.length; i++) {
+    // console.log(bombs[i]);
+
+    const bombElement = bombs[i];
+    console.log(bombElement);
+
+    // bombElement.addEventListener('click', function () {
+    //     bombElement.classList.add('red');
+    // })
 
 }
+
+
+function getRandomIntInclusive(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+
+
+
 
